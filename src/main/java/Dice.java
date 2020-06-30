f9b0a850015a6aa731a0dbdbbcb8643624ca99bf
Thabiso <Thabiso.Seekane@umuzi.org>
2020-06-30 15:45:11 +0200

    Refactored my code to handle probabilities

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -4,27 +4,62 @@ public class Dice {
 
     // local variables.
     private int numOfSides;
-    private int Value;
+    private int value;
+
+    Random random;
+    private  int[] probabilities;
 
     public Dice(int numberOfSides)
     {
-        numOfSides = numberOfSides;
-        rollDice();
+        this.numOfSides = numberOfSides;
     } // end of constructor.
+
+    public void value(int value){
+        this.value = value;
+    }
+
+    public Dice(int numOfSides, int[] probabilities){
+        this.numOfSides = numOfSides;
+        int i = 0;
+        while(i < probabilities.length){
+            int prob = probabilities[i];
+            if (prob < 0){
+                System.out.println("Negative probabilities: Not Allowed.");
+            }
+            else if (prob != probabilities[i]){
+                System.out.println("ERROR : Value Not Allowed Enter Only Integers");
+            }
+            else if (prob == 0){
+                System.out.println("ERROR : probabilities Must Be greater than Zero");
+            }
+            else if (prob == probabilities[i]){
+                numOfSides = probabilities.length;
+            }
+            i++;
+        }
+    }
 
     public void rollDice()
     {
         Random r = new Random(); // random object.
-        Value = r.nextInt(numOfSides) + 1; // shifting Range of dice from 0 - 5 to 1 - 6
+        if (probabilities == null){
+            int i = 0;
+            while(i < numOfSides){
+                int Roll = random.nextInt(numOfSides) + 1; // shifting Range of dice from 0 - 5 to 1 - 6
+                value(Roll);
+                i++;
+            }
+        }
+
     } // end of method rollDice.
 
-    public int getValue()
-    {
-        return Value;
-    }// end of method getValue
-
-    public int getNumOfSides()
-    {
-        return numOfSides;
-    }// end of method getNumOfSides.
+//    public int getValue()
+//    {
+//        return value;
+//    }// end of method getValue
+//
+//    public int getNumOfSides()
+//    {
+//        return numOfSides;
+//    }// end of method getNumOfSides.
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        Dice die = new Dice(12);
+        Dice die = new Dice(20);
 
-        System.out.println(die.getNumOfSides() + " sided die is:");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Roll " + (i+1) +": " + die.getValue());
-            die.rollDice();
-        }
+        die.rollDice();
+        Dice die6 = new Dice(0,new int[]{1,1,1,1,1,2});
+        die6.rollDice();
+        System.out.println(die6);
+        System.out.println(die);
+ //        System.out.println(die.getNumOfSides() + " sided die is:");
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("Roll " + (i+1) +": " + die.getValue());
+//            die.rollDice();
+//        }
 
 
 /*            die.rollDice();
